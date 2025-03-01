f95a77d7f025abdcf3086cabd4005455a3aafc1d
Elvis Mbugua <manfuturekahande@gmail.com>
2025-03-01 09:24:02 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,21 @@
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
+
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm so excited to learn stuff.")
+        print("Oh, by the way, did you see the latest episode of my favorite show?")
+        print("I can't believe who died...")  # Ensure exactly three dots
+
+    def raise_hand(self):
+        for _ in range(10):
+            print("Pick me!")
+           
+
+
